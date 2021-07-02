a04a9dd510bac1e75328718e77e6368866289b4e
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-07-02 17:41:09 +0300

    mtd: spi-nor: manuf-id-collisions: Add support for xt25f128b
    Flash does not support continuation codes and may collide with a flash
    of other manufacturer, Intersil being an example .
    
    This flash addition is just for demonstration purposes. As I don't
    have the flash, I assumed that it supports SFDP.
    If the flash does not define the SFDP tables, it should be statically
    initialized with:
            /* XTX (XTX Technology Limited) */
            { "XT25F128B", INFO(0x0b4018, 0, 64 * 1024, 256, SPI_NOR_SKIP_SFDP |
                                SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
                                SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
    };
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/manuf-id-collisions.c
+++ drivers/mtd/spi-nor/manuf-id-collisions.c
@@ -8,6 +8,10 @@ static const struct flash_info id_collision_parts[] = {
 	{ "by25q128as", INFO(0x684018, 0, 64 * 1024, 256, SPI_NOR_SKIP_SFDP |
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			     SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
+
+	/* XTX (XTX Technology Limited) */
+	{ "xt25f128b", INFO(0x0b4018, 0, 64 * 1024, 256, SPI_NOR_PARSE_SFDP |
+			    SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
 };
 
 const struct spi_nor_manufacturer spi_nor_manuf_id_collisions = {
